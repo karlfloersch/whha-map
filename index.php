81a7c2ca1b480e2de86ffadeb5207134821680fa
karlfloersch <karl.floersch@gmail.com>
2014-05-01 00:58:05 -0400

    Removed width and height elements from canvas

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 
 <div id="map-container">
 	<!-- <canvas id="map-canvas" width="935" height="770"> -->
-	<canvas id="map-canvas" width="90%" height="50%">
+	<canvas id="map-canvas">
 	</canvas>
 	
 </div>
